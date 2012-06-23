3fbeea0d4eab57c2f44ae51ad077381e1314a327
Alexandru Scvortov <scvalex@gmail.com>
2012-06-23 15:29:05 +0100

    more fleshing out for VC.Approximate

--- Data/VectorClock/Approximate.hs
+++ Data/VectorClock/Approximate.hs
@@ -8,9 +8,15 @@ module Data.VectorClock.Approximate (
         VectorClock,
         -- * Construction
         empty, singleton, fromList,
+        -- * Query
+        null, size, member, lookup,
         -- * Insertion
-        insert
+        insert, inc, incWithDefault,
+        -- * Deletion
+        delete
     ) where
+
+import Prelude hiding ( null, lookup )
 
 import Data.Binary ( Binary(..) )
 import Data.Hashable ( Hashable(..) )
@@ -26,30 +32,30 @@ import qualified Data.VectorClock.Simple as VC
 -- show that approximate vector clocks have good results in practice.
 -- See the paper by R. Baldoni and M. Raynal for details.
 data VectorClock a b = VectorClock
-    { clock :: VC.VectorClock Int b
-    , size  :: Int
+    { vcClock :: VC.VectorClock Int b
+    , vcSize  :: Int
     }
 
 instance (Eq b) => Eq (VectorClock a b) where
-    vc1 == vc2 = clock vc1 == clock vc2
+    vc1 == vc2 = vcClock vc1 == vcClock vc2
 
 instance (Show b) => Show (VectorClock a b) where
-    show = show . clock
+    show = show . vcClock
 
 instance (Binary b) => Binary (VectorClock a b) where
     put vc = do
-        put (clock vc)
-        put (size vc)
+        put (vcClock vc)
+        put (vcSize vc)
     get = do
         xys <- get
         k <- get
-        return (VectorClock { clock = xys, size = k })
+        return (VectorClock { vcClock = xys, vcSize = k })
 
 -- | /O(1)/.  The empty vector clock.
 empty :: Int                    -- ^ /size/: the maximum number of
                                 -- entries in the vector clock
       -> VectorClock a b
-empty k = VectorClock { clock = VC.empty, size = k }
+empty k = VectorClock { vcClock = VC.empty, vcSize = k }
 
 -- | /O(N)/.  Insert each entry in the list one at a time.
 fromList :: (Hashable a)
@@ -69,13 +75,57 @@ singleton :: (Hashable a)
           -> VectorClock a b
 singleton k x y = fromList k [(x, y)]
 
+-- | /O(1)/.  Is the vector clock empty?
+null :: VectorClock a b -> Bool
+null = VC.null . vcClock
+
+-- | /O(N)/.  The number of entries in the vector clock.  Note that
+-- this may be less than the /size/ at construction.
+size :: VectorClock a b -> Int
+size = VC.size . vcClock
+
+-- | /O(N)/.  Is the given key a key in an entry of the vector clock?
+member :: (Hashable a) => a -> VectorClock a b -> Bool
+member x (VectorClock { vcClock = xys, vcSize = k }) =
+    VC.member (mapKey x k) xys
+
+-- | /O(N)/.  Lookup the value for a key in the vector clock.
+lookup :: (Hashable a)  => a -> VectorClock a b -> Maybe b
+lookup x (VectorClock { vcClock = xys, vcSize = k }) =
+    VC.lookup (mapKey x k) xys
+
 -- | /O(N)/.  Insert or replace the entry for a key.
-insert :: (Hashable a)
-       => a                     -- ^ /key/: the key for the entry
-       -> b                     -- ^ /value/: the value for the entry
-       -> VectorClock a b       -- ^ /vc/: the vector clock in which
-                                -- to insert
-       -> VectorClock a b
-insert x y vc@(VectorClock { clock = xys, size = k }) =
-    let xys' = VC.insert (hash x `mod` k) y xys in
-    vc { clock = xys' }
+insert :: (Hashable a) => a -> b -> VectorClock a b -> VectorClock a b
+insert x y vc@(VectorClock { vcClock = xys, vcSize = k }) =
+    let xys' = VC.insert (mapKey x k) y xys in
+    vc { vcClock = xys' }
+
+-- | /O(N)/.  Increment the entry for a key.
+inc :: (Hashable a, Num b) => a -> VectorClock a b -> Maybe (VectorClock a b)
+inc x vc@(VectorClock { vcClock = xys, vcSize = k }) = do
+    xys' <- VC.inc (mapKey x k) xys
+    return (vc { vcClock = xys' })
+
+-- | /O(N)/.  Increment the entry for a key.  If the key does not
+-- exist, assume it was the default.
+incWithDefault :: (Hashable a, Num b)
+               => a               -- ^ /key/: the key of the entry
+               -> VectorClock a b -- ^ /vc/: the vector clock
+               -> b               -- ^ /default/: if the key is not
+                                  -- found, assume its value was the
+                                  -- default and increment that
+               -> VectorClock a b
+incWithDefault x vc@(VectorClock { vcClock = xys, vcSize = k }) y' =
+    let xys' = VC.incWithDefault (mapKey x k) xys y' in
+    vc { vcClock = xys' }
+
+-- | /O(N)/.  Delete an entry from the vector clock.  If the requested
+-- entry does not exist, does nothing.
+delete :: (Hashable a) => a -> VectorClock a b -> VectorClock a b
+delete x vc@(VectorClock { vcClock = xys, vcSize = k }) =
+    let xys' = VC.delete (mapKey x k) xys in
+    vc { vcClock = xys' }
+
+-- | Map a key into the domain of approximate keys.
+mapKey :: (Hashable a) => a -> Int -> Int
+mapKey x k = hash x `mod` k
